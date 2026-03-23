e81da43b1e31a4ad793d702fa4d6bcf622ac486f
Joumana Mohamed <s-joumana.yousef@zewailcity.edu.eg>
2026-03-23 19:09:18 +0200

    Update train.py

--- train.py
+++ train.py
@@ -11,19 +11,20 @@ mlflow.set_tracking_uri(tracking_uri)
 mlflow.set_experiment("assignment5-classifier")
 
 X, y = load_wine(return_X_y=True)
+
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
     test_size=0.2,
     random_state=42,
-    stratify=y,
+    stratify=y
 )
 
 with mlflow.start_run() as run:
     model = RandomForestClassifier(
         n_estimators=300,
         max_depth=None,
-        random_state=42,
+        random_state=42
     )
     model.fit(X_train, y_train)
 
